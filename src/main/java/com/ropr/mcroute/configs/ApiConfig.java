5d7b135576b8e189cc9153b1c993fa8d83ce1305
Niklas Johansson <niklas.j.johansson@sigma.se>
2016-06-28 12:55:06 +0200

    test igen

--- src/main/java/com/ropr/mcroute/configs/ApiConfig.java
+++ src/main/java/com/ropr/mcroute/configs/ApiConfig.java
@@ -4,8 +4,8 @@ package com.ropr.mcroute.configs;
  * Created by NIJO7810 on 2016-05-04.
  */
 public class ApiConfig {
-    public static final String API_ENDPOINT = "API_ENDPOINT_PATH";
-    public static final String API_CLIENT = "API_CLIENT_NAME";
-    public static final String API_SECRET = "API_SECRET";
-    public static final String API_REDIRECT_URL = "API_CALLBACK_PATH";
+    public static final String API_ENDPOINT = "http://mcrutt.se/api/";
+    public static final String API_CLIENT = "mcrutt.android.se";
+    public static final String API_SECRET = "vKNXvjnMJyk4HJZvUo2u";
+    public static final String API_REDIRECT_URL = "http://mcrutt.se/authcomplete.html";
 }
