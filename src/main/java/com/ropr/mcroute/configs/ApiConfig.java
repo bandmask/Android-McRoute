3fbf06550af6f8da9b7034f0d4aeeb76f40c2f81
Niklas Johansson <niklasjsson@gmail.com>
2016-06-28 12:51:32 +0200

    Create ApiConfig.java

---
+++ src/main/java/com/ropr/mcroute/configs/ApiConfig.java
@@ -0,0 +1,11 @@
+package com.ropr.mcroute.configs;
+
+/**
+ * Created by NIJO7810 on 2016-05-04.
+ */
+public class ApiConfig {
+    public static final String API_ENDPOINT = "API_ENDPOINT_PATH";
+    public static final String API_CLIENT = "API_CLIENT_NAME";
+    public static final String API_SECRET = "API_SECRET";
+    public static final String API_REDIRECT_URL = "API_CALLBACK_PATH";
+}
